ef66c756f8c48e1588d2d0315de6438738e82bab
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2012-12-14 03:03:52 +0900

    gemnize

--- mrblib/growthforecast.rb
+++ mrblib/growthforecast.rb
@@ -306,20 +306,3 @@ class GrowthForecast
     end
   end
 end
-
-g = GrowthForecast::Client.new("127.0.0.1", 5125)
-
-config = {
-  :service  => "apache", 
-  :section  => "scoreboard",
-  :gragh    => "woreker",
-}
-
-data = {
-  :number   => 5, 
-  :color    => "#333399",
-  :mode     => "count",
-}
-
-p g.post(config, data)
-
