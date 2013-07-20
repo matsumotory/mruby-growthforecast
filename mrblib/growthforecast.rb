560802fbc801ed9ff742e2a971641737b11d63d0
y-ken <y.ken.studio@gmail.com>
2013-07-20 22:18:11 +0900

    typo fixed

--- mrblib/growthforecast.rb
+++ mrblib/growthforecast.rb
@@ -3,20 +3,20 @@ class GrowthForecast
   DEFAULTPORT     = 5125
   class Client
     def initialize(address, port)
-      adress = DEFAULTADDRESS if adress.nil?
-      port   = DEFAULTAPORT if adress.nil?
+      address = DEFAULTADDRESS if address.nil?
+      port   = DEFAULTPORT if address.nil?
       @url = sprintf("http://%s:%s/api", address, port.to_s)
     end
 
     def post(config, data)
       http = HttpRequest.new()
-      api_url = sprintf("%s/%s/%s/%s", @url, config[:service], config[:section], config[:gragh])
+      api_url = sprintf("%s/%s/%s/%s", @url, config[:service], config[:section], config[:graph])
       http.post(api_url, {
         :number => data[:number],
         :color  => data[:color],
         :mode   => data[:mode],
       },{
-        'User-Agent' => "mruby-growthforcast",
+        'User-Agent' => "mruby-growthforecast",
         "Content-Type" => 'application/x-www-form-urlencoded',
       })
     end
